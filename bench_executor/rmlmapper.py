7bbf8b557c6ca1275a0fbc6acee454006e7cde44
Dylan Van Assche <dylan.vanassche@ugent.be>
2024-01-04 09:41:18 +0100

    {executor, rmlmapper}: fix linting

--- bench_executor/rmlmapper.py
+++ bench_executor/rmlmapper.py
@@ -12,7 +12,6 @@ import os
 import psutil
 from typing import Optional
 from timeout_decorator import timeout, TimeoutError  # type: ignore
-from bench_executor.container import Container
 from bench_executor.logger import Logger
 
 VERSION = '6.0.0'
