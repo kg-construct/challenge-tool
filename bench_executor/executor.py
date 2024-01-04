9af8447fb410a529340f0e27388190754cf3a992
Dylan Van Assche <dylan.vanassche@ugent.be>
2024-01-04 10:21:39 +0100

    {exectool, executor} fix more linting problems

--- bench_executor/executor.py
+++ bench_executor/executor.py
@@ -396,8 +396,9 @@ class Executor:
                 self._progress_cb('Initializing', step['resource'], success)
 
         # Launch metrics collection
-        collector = Collector(results_run_path, interval, len(data['steps']),
-                              run, directory, self._verbose)
+        collector = Collector(data['name'], results_run_path, interval,
+                              len(data['steps']), run, directory,
+                              self._verbose)
 
         # Execute steps
         for index, step in enumerate(data['steps']):
@@ -499,13 +500,14 @@ class Executor:
                 if parameters.get('output_file', False) \
                         and not parameters.get('multiple_files', False):
                     output_dir = os.path.join(results_run_path, subdir)
-                    for f in glob(os.path.join(directory, 'data',
-                                               'shared', '*.nt')):
-                        p = os.path.join(output_dir, os.path.basename(f))
+                    for out_file in glob(os.path.join(str(directory), 'data',
+                                         'shared', '*.nt')):
+                        out_path = os.path.join(output_dir,
+                                                os.path.basename(out_file))
                         try:
-                            shutil.move(f, p)
+                            shutil.move(out_file, out_path)
                         except FileNotFoundError as e:
-                            msg = f'Cannot find output file "{f}": {e}'
+                            msg = f'Cannot find output file "{out_file}": {e}'
                             self._logger.warning(msg)
 
             # Run complete, mark it
